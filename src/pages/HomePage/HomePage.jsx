ba2316b46b15cb816fdee6c2aa6500550f8b1395
Cemrdeniz <blackviens@outlook.com>
2026-01-29 18:38:00 +0300

    update

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,25 +1,24 @@
 import Header from "../../components/Layout/Layout";
 import styles from "./HomePage.module.css";
 import heroImg from "../../assets/hero.jpg";
+import { Link } from "react-router-dom";
 
 export default function HomePage() {
   return (
     <div className={styles.page}>
       <Header />
 
-      <main
-        className={styles.hero}
-        style={{ "--hero": `url(${heroImg})` }}
-      >
+      <main className={styles.hero} style={{ "--hero": `url(${heroImg})` }}>
         <div className={styles.container}>
           <div className={styles.content}>
             <h1 className={styles.title}>Campers of your dreams</h1>
             <p className={styles.text}>
               You can find everything you want in our catalog
             </p>
-            <a className={styles.button} href="/catalog">
+
+            <Link className={styles.button} to="/catalog">
               View Now
-            </a>
+            </Link>
           </div>
         </div>
       </main>
